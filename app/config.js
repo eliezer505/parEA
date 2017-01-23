aa4039d84349f24121a9f2ae44071008d81f988a
arama <aramainfo@gmail.com>
2017-01-23 23:31:12 +0200

    23.1.17

--- app/config.js
+++ app/config.js
@@ -1,7 +1,11 @@
 (function() {
 
- angular.module("clubear",['ngRoute']);
-
+ var club = angular.module("clubear",['ngRoute','socialLogin']);
+club.config(function(socialProvider){
+    //socialProvider.setGoogleKey("YOUR GOOGLE CLIENT ID");
+  //socialProvider.setLinkedInKey("YOUR LINKEDIN CLIENT ID");
+  socialProvider.setFbKey({appId: "569349916606060", apiVersion: "v2.8"});
+});
 
 })();
 
